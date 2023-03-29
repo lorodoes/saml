d63265e0fd534aefcbe4c9012cfbbefbe59d994b
Garrod Alwood <galwood@smartronix.com>
2023-03-29 19:33:39 -0400

    Updating to lorodoes

--- identity_provider_test.go
+++ identity_provider_test.go
@@ -26,9 +26,9 @@ import (
 	"github.com/beevik/etree"
 	"github.com/golang-jwt/jwt/v4"
 
-	"github.com/crewjam/saml/logger"
-	"github.com/crewjam/saml/testsaml"
-	"github.com/crewjam/saml/xmlenc"
+	"github.com/lorodoes/saml/logger"
+	"github.com/lorodoes/saml/testsaml"
+	"github.com/lorodoes/saml/xmlenc"
 )
 
 type IdentityProviderTest struct {
